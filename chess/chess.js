b77dd08a11c4ec201429545078eccbd4503a95ac
sanchibareja <sanchibareja@gmail.com>
2026-06-28 19:11:37 +0530

    update

--- chess/chess.js
+++ chess/chess.js
@@ -1,2 +1,29 @@
+let chessBoard = document.getElementById("chess-board");
+chessBoard.classList = "p-10 ";
 
+let boardSize = 8;
+
+function newLine(){
+    let br = document.createElement("br");
+    chessBoard.appendChild(br);
+}
+
+function print(data){
+    let box = document.createElement("div");
+    box.innerText = data;
+    box.className = "w-10 h-10 text-5xl display: inline-block";
+    chessBoard.appendChild(box);
+}
+
+for(let i=1;i<=boardSize;i++){
+    for(let j=1;j<=boardSize;j++){
+        if(((i+j)%2)==0){
+            print("⬛️");
+        }
+        else{
+            print("⬜️");
+        }
+    }
+    newLine();
+}
 //baljeet.singh@codequotient.com
